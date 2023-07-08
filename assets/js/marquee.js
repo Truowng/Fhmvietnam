9eb8f0017138021456421d49197274245afe00f3
Truowng <quockhanhtruong166@gmail.com>
2023-07-08 17:32:20 +0700

    optimize mobile client infinite slide

--- assets/js/marquee.js
+++ assets/js/marquee.js
@@ -1,5 +1,18 @@
 const projectSlides = document.querySelectorAll("#project .project-slide");
-projectSlides.forEach((projectSlide) => {
-  let clone = projectSlide.querySelector(".project-slide-item").cloneNode(true);
-  projectSlide.appendChild(clone);
-});
+const clientSlides = document.querySelectorAll(
+  "#client .client-desktop .client-slide"
+);
+const clientSlidesM = document.querySelectorAll(
+  "#client .client-mobile .client-slide"
+);
+
+const clone = (parentEl, childEl) => {
+  parentEl.forEach((item) => {
+    let clone = item.querySelector(`.${childEl}`).cloneNode(true);
+    item.appendChild(clone);
+  });
+};
+
+clone(projectSlides, "project-slide-item");
+clone(clientSlides, "client-slide-list");
+clone(clientSlidesM, "client-slide-list");
